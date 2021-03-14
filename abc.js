74ab47e26cd3b7ef1defefcb8f128b1351ea9c6b
aniket6505 <aniketchavan311@gmail.com>
2021-03-14 15:50:29 +0530

    added new files

---
+++ abc.js
@@ -0,0 +1,15 @@
+// console.log("abc");
+// console.log("abc");
+// console.log("abc");
+// console.log("abc");
+// console.log("abc");
+Number=10
+if(Number>10)
+{
+    console.log("Number is greater than ten");
+}elseif{
+    console.log("number is ten")
+}
+else{
+    console.log("number is less than ten");
+}
---
+++ datatypes.js
@@ -0,0 +1,16 @@
+// var Name="Aniket";
+// console.log("Name");
+
+// num=2;
+// while(num>=0)
+// console.log("number is greater than zero");
+// num=num-1;
+
+// number=955;
+// if(number%2===0)
+// {
+//     console.log("number is even")
+// }
+// else{
+//     console.log("number is odd")
+// }
