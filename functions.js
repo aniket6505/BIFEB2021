5dd97806b6bc96939f85e92cfe102ad527bcf62d
aniket6505 <aniketchavan311@gmail.com>
2021-03-22 19:05:11 +0530

    added function program

---
+++ functions.js
@@ -0,0 +1,25 @@
+// function syntax
+// function name_of_the_function(){
+    // statement-1
+    // statement-2
+    // statement-3
+    // statement-4
+// }
+var num=5
+if(num==5){
+    console.log("its five")
+    printNum()
+}else{
+    printAlpha()
+    console.log("done")
+}
+function printNum(){
+    console.log("1")
+    console.log("2")
+   
+}
+
+function printAlpha(){
+    console.log("AN")
+    console.log("NA")
+}
