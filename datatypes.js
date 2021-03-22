d0b49d3b4af857d2933c683586d734bab490fac8
aniket6505 <aniketchavan311@gmail.com>
2021-03-22 18:18:22 +0530

    added new datatypes files

--- datatypes.js
+++ datatypes.js
@@ -14,3 +14,147 @@
 // else{
 //     console.log("number is odd")
 // }
+
+// for(var i=0; i<4 ;i++){
+//     console.log("hello X"+i)
+// }
+// console.log("done for now")
+
+// month=3
+// switch(month){
+//     case 1 :
+//         console.log("winter")
+//         console.log("jan")
+//         break;
+//     case 2 :
+//         console.log("winter")
+//         console.log("feb")
+//         break;
+//     case 3 :
+//         console.log("winter")
+//         console.log("march")
+//         break;
+//     default :
+//      console.log("we have 3 months of data")
+//      console.log("Thank you")
+// }
+
+// ternary operator
+// var marks=35
+// var result= marks<40; "fail" : "pass"
+// console.log("result")
+
+
+// Practical
+// var isValid="Aniket";
+// isValid=true;
+// isValid=10
+// var name="abc";
+// console.log(name)
+
+
+// var student={Name:"Aniket",Address:"Google",Rollnumber:10,isGraduated: false};
+// console.log(student)
+
+// if condition
+// var number=7
+// if (number%2==0) {
+//     console.log(number+ " is even number")
+// }
+// if (number%2!=0){
+//     console.log(number+ " is odd number")
+// }
+
+
+// if-else if 
+// var number=111
+// if (number%2==0) {
+//     if (number>10) {
+//         console.log(number + " greater than 10")
+//     }else if(number<10) {
+//         console.log(number + " less than 10")
+//     }else{
+//     console.log(number + " equal to 10")
+//       }
+//     console.log(number + " is even number")
+// }else{
+//     if (number>10) {
+//         console.log(number + " greater than 10")
+//     }else if(number<10) {
+//         console.log(number + " less than 10")
+//     }else{
+//     console.log(number + " equal to 10")
+//     }
+//     console.log(number + " is odd number")
+// }
+
+
+// while loop
+// count=2
+// while (count<0) {
+//     console.log(" Hello moto")
+//     count=count-1;
+//     console.log(count + " count value ")
+// }
+
+// do while loop
+// var count=2
+// do {
+//      console.log(" Hello moto")
+//      count=count-1;
+//      console.log(count + " count value ")
+// }while (count<0)
+
+// practice 16march 2021
+// var count;
+// a=5
+// b=5
+// c=null
+// count=a+b;
+// console.log("sum is " +count)
+// if (count>10) {
+//     console.log("number is greater than ten")
+// }else if(count>=10){
+//     console.log("number is greater than equal to ten")
+// }else{
+//     console.log("number is less than ten")
+// }
+// count=count+count;
+// console.log("sum is now " +count)
+// while (count>0) {
+//     console.log("count is " +count)
+//     count=count-1
+// }
+
+// for loop
+// syntax- for(exp1;exp2;exp){
+//     statement1
+//     statement2
+//     ...
+// }
+
+// for(var i=0; i<4; i++){
+// console.log("hello")
+// }
+// console.log("done")
+
+
+// switch statement
+month=3
+switch(month){
+    case 1:
+        console.log("winter")
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("winter")
+        console.log("feb")
+        break;
+    case 3:
+        console.log("lil winter")
+        console.log("march")
+        break;
+    default :
+        console.log("we have only three months data")
+        console.log("only jan, feb and march")
+}
