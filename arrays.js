8c3c960999482e6dd38b4446030c9e79fc911097
aniket6505 <aniketchavan311@gmail.com>
2021-03-27 17:49:25 +0530

    task added

---
+++ arrays.js
@@ -0,0 +1,4 @@
+var named=["aniket", "sanket", "shubham", 10]
+console.log(named[3])
+named.push("satyam")
+console.log(named)
---
+++ varletconst.js
@@ -0,0 +1,21 @@
+/*  3 ways of defining variable
+ 1. var - global 
+ let - local to the block
+
+
+*/
+// for(var i=0; i<5;i++){
+//     console.log(i)
+// }
+// console.log(i)
+
+
+// let ex
+// for(let i=0; i<5;i++){
+//     console.log(i)
+// }
+// console.log(i)
+
+// const ex
+const a=10
+console.log(a)
